a992afbb13c2c8ce7ecdc55839bcc6cfc5bf696f
Eric Yung <94202489+EricYung02@users.noreply.github.com>
2025-04-22 16:55:13 -0400

    made it so that runNet doesnt need a parameter

--- mc_interface/autorun.py
+++ mc_interface/autorun.py
@@ -1,6 +1,7 @@
 """
 Script that each minescript agent will autorun upon start
 """
+import numpy as np
 
 from minekour.PlayerManager import PlayerManager, init
 from minekour.networkReceiver import networkReceiver
@@ -27,17 +28,16 @@ def kill():
     execute("\killjob")
     raise SystemExit(0)
 
-def modifyNet(recieved_genes) -> ControlNeuralNetwork:
+def modifyNet(recieved_genes):
     """
     Modifies neural net and returns new neural net from genes recieved
     """
-    return ControlNeuralNetwork.from_genes(
+    net =  ControlNeuralNetwork.from_genes(
         genes=recieved_genes,
         hidden_layer_sizes=hidden_layer_sizes,
-        radial_distance=radial_distance
-    )
+        radial_distance=radial_distance)
 
-def runNet(net: ControlNeuralNetwork):
+def runNet():
     """
     runs neural network and controls the player with the output
     """
@@ -75,9 +75,21 @@ def stopNet():
 hidden_layer_sizes = [64, 32]
 radial_distance = 6
 output_size = 8
+gene_size = ControlNeuralNetwork.get_gene_size(hidden_layer_sizes, radial_distance)
+# Generate random weights (genes)
+input_dim = (2*radial_distance+1)**3 + 2
+scaling_factor = 1/np.sqrt(input_dim)
 
 # run starts here
 init()
+
+random_genes = [np.random.normal(0, scaling_factor) for _ in range(gene_size)]
+net = ControlNeuralNetwork.from_genes(
+    genes=random_genes,
+    hidden_layer_sizes=hidden_layer_sizes,
+    radial_distance=radial_distance
+)
+
 networkReceiver.initCallbacks(set_val=modifyNet, 
                               run_model=runNet, 
                               stop_model=stopNet, 
